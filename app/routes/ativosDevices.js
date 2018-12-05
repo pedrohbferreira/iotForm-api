ca900d502a11e5386ea110164d011cd0fa473df8
pedrohbferreira <pedro.barreto@outlook.com>
2018-12-05 04:09:32 -0200

    correção de referência e regex

--- app/routes/ativosDevices.js
+++ app/routes/ativosDevices.js
@@ -1,11 +1,11 @@
 module.exports = function(app) {
-    var ativosDevicesController = app.controllers.ativos;
+    var ativosDevicesController = app.controllers.ativosDevices;
 
     app.route('/ativosDevices')
     .get(ativosDevicesController.getAtivos)
     .post(ativosDevicesController.postAtivos);
 
-    app.route('/ativos/:id[0-9]{1,4})')
+    app.route('/ativos/:id([0-9]{1,4})')
     .get(ativosDevicesController.getAtivosId)
     .put(ativosDevicesController.putAtivos)
     .delete(ativosDevicesController.deleteAtivos);
